642c252de1e76f2aca70e562f1d43db01aacf160
Hamid Raza <hamidraza.engr@gmail.com>
2024-08-13 00:02:10 +0500

    hamid Raza portfolio

---
+++ android/app/src/main/kotlin/dev/davidcobbina/portfolio_site/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.hamidraza.portfolio_site
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/dev/davidcobbina/portfoliosite/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.hamidraza.portfoliosite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
